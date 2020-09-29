06312ff4808106eb9c802145508b492a3b1fddb4
xipeng156 <1562107250@qq.com>
2020-09-29 20:23:11 +0800

    注册-后端完成

--- src/main/java/com/qst/controller/UserController.java
+++ src/main/java/com/qst/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.qst.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.qst.enums.UserStatus;
 import com.qst.pojo.User;
 import com.qst.service.UserService;
+import com.qst.util.MD5;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @Author: XiPeng
@@ -20,18 +26,25 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
     @ResponseBody
-    public String register(@RequestParam(value = "loginName") String userName,
-                           @RequestParam(value = "password") String password,
-                           @RequestParam(value = "userName") String roleName,
-                           @RequestParam(value = "phone") String phone){
+    public String register(HttpSession session,
+                           @RequestParam(value = "loginName",required = false) String userName,
+                           @RequestParam(value = "password",required = false) String password,
+                           @RequestParam(value = "userName",required = false) String roleName,
+                           @RequestParam(value = "phone",required = false) String phone){
         User user = new User();
         user.setUsername(userName);
         user.setPassword(password);
         user.setRealname(roleName);
         user.setPhone(phone);
-
+        int register = userService.register(user);
+        if (register==1){
+            session.setAttribute("status",JSON.toJSONString(UserStatus.REGISTER_SUCCESS));
+        }
+        else {
+            session.setAttribute("status",JSON.toJSONString(UserStatus.REGISTER_FAIL));
+        }
 
         return "";
     }
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,35 @@
+import com.alibaba.fastjson.JSON;
+import com.qst.controller.UserController;
+import com.qst.enums.UserStatus;
+import com.qst.pojo.User;
+import com.qst.service.UserService;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: XiPeng
+ * @Description:
+ * @Date: Create in 20:03 2020/9/29
+ * @Modified by:
+ */
+public class UserTest {
+    @Test
+    public void registerTest(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
+        UserService service = context.getBean(UserService.class);
+        User user = new User();
+        user.setUsername("123");
+        user.setPassword("123");
+        user.setPhone("121212");
+        user.setRealname("张三");
+        int register = service.register(user);
+        if (register==1){
+            System.out.println(JSON.toJSONString(UserStatus.REGISTER_SUCCESS));
+        }
+        else {
+            System.out.println(JSON.toJSONString(UserStatus.REGISTER_FAIL));
+        }
+
+
+    }
+}
